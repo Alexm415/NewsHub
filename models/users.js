8321c844ac9cb293b354d63097f72a2068cfca98
tina ra <tina.rahimian94@gmail.com>
2024-11-14 19:48:30 -0800

    users.js

--- models/users.js
+++ models/users.js
@@ -1,49 +1,63 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-const sequelize = new Sequelize('database_name', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-const User = sequelize.define('User', {
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, // Ensure usernames are unique
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, // Ensure emails are unique
-        validate: {
-            isEmail: true, // Validate that the input is a valid email format
-        },
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false, // Password should not be null
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
     },
     location: {
-        type: DataTypes.STRING,
-        allowNull: true, // Location can be optional
+                type: DataTypes.STRING,
+                allowNull: true, // Location can be optional
+            },
+    
+  
+
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
     },
-});
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users',
+  }
+);
 
-const bcrypt = require('bcrypt');
-
-const createUser = async (username, email, password, location) => {
-    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-    User.create({
-        username: username,
-        email: email,
-        password: hashedPassword,
-        location: location,
-    })
-    .then(user => {
-        console.log('User created:', user);
-    })
-    .catch(error => {
-        console.error('Error creating user:', error);
-    });
-};
+module.exports = User;
